e536f620aea92b4b11932dfe7ee94e96e416cd5f
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-08-21 12:34:16 +0300

    Update

---
+++ MemoryGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+
+{
+
+    [SerializeField] private Sprite bgImage;
+
+    public List<Button> btns =new List<Button> ();
+
+    private void Start()
+    {
+        GetButtons ();
+    }
+
+    private void GetButtons()
+    {
+        GameObject [] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+
+
+        for(int i =0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+        }
+    }
+}
